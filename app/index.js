d50f190d80a7e0fc5c195a60f5e08d8b07f75cb4
Gaya K K <gaya.kk@cybrilla.com>
2018-07-01 01:56:53 +0530

    spotlight component and styles

--- app/index.js
+++ app/index.js
@@ -4,4 +4,9 @@ import 'slick-carousel'
 import '../node_modules/slick-carousel/slick/slick.css'
 import '../scss/app.scss'
 $(document).ready(function() {
+  $('.spotlight__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
 }())
